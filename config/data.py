4059cdb157f3c9668879dcb36487d3c27d17af30
ArtDmSav <artdmsav@gmail.com>
2024-07-04 14:19:31 +0300

    add sentry

--- config/data.py
+++ config/data.py
@@ -1,6 +1,8 @@
 import configparser
 from pathlib import Path
 
+import sentry_sdk
+
 # Absolut path
 dir_path = Path.cwd()
 path = Path(dir_path, 'config', 'config.ini')
@@ -23,13 +25,13 @@ ADMIN_USERNAME = (config['Admin']['admin_1'], config['Admin']['admin_2'], config
 
 # Sentry
 
-# sentry_sdk.init(
-#     dsn=config['Sentry']['sentry_dsn'],
-#     # Set traces_sample_rate to 1.0 to capture 100%
-#     # of transactions for performance monitoring.
-#     traces_sample_rate=1.0,
-#     # Set profiles_sample_rate to 1.0 to profile 100%
-#     # of sampled transactions.
-#     # We recommend adjusting this value in production.
-#     profiles_sample_rate=1.0,
-# )
+sentry_sdk.init(
+    dsn=config['Sentry']['sentry_dsn'],
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
